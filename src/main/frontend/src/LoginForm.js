73e5ad3663715d5f175fbbd99f562fc93af66575
Lisiasty <sonicmephit@gmail.com>
2024-06-11 17:21:52 +0200

    Meeting participants list fix

--- src/main/frontend/src/LoginForm.js
+++ src/main/frontend/src/LoginForm.js
@@ -2,6 +2,24 @@ import {useState} from "react";
 
 export default function LoginForm({onLogin, buttonLabel}) {
     const [email, setEmail] = useState('');
+    const [login, setLogin] = useState('');
+
+//        async function handleRegister(participant) {
+//            const response = await fetch(`/participants`, {
+//                method: 'POST',
+//                body: JSON.stringify({login: username, password: password}),
+//                headers: { 'Content-Type': 'application/json' }
+//            });
+//            if (response.ok) {
+//                const nextMeetings = meetings.map(m => {
+//                    if (m === meeting) {
+//                        m.participants = [...m.participants, username];
+//                    }
+//                    return m;
+//                });
+//                setMeetings(nextMeetings);
+//            }
+//        }
 
     return <div>
         <label>Zaloguj się e-mailem</label>
@@ -9,8 +27,8 @@ export default function LoginForm({onLogin, buttonLabel}) {
         <button type="button" onClick={() => onLogin(email)}>{buttonLabel || 'Wchodzę'}</button>
 
          <label>Zaloguj się loginem i hasłem</label>
+         <input type="text" value={login} onChange={(e) => setLogin(e.target.value)}/ />
          <input type="text" />
-         <input type="text" />
-         <button type="button" ></button>
+         <button type="button" >{buttonLabel || 'Wchodzę'}</button>
     </div>;
 }
